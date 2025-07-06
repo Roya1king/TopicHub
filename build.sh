a59874a154e77a44de85cbf3e94b952faa682156
Royal1king <theroyalking112233@gmail.com>
2025-07-06 19:29:15 +0530

    deploymet

--- build.sh
+++ build.sh
@@ -5,8 +5,10 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Migrate DB
+# Run database setup
+python manage.py makemigrations
 python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --no-input
+
